22eab1cdfb2804c1d3fdec78ce465b07c7158ecf
Koussai21 <koussailaribi21@gmail.com>
2024-02-22 15:53:21 +0100

    Ex6: add / and /conversation routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,28 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-
-// 1. Define route components.
-// These can be imported from other files
-const Home = { template: '<div>Home</div>' }
-const Conversation = { template: '<div>Conversation</div>' }
+import Home from '../pages/HomePage.vue'
+import Conversation from '../pages/ConversationIndexPage.vue'
 
 // 2. Define some routes
 // Each route should map to a component.
 // We'll talk about nested routes later.
 const routes = [
-  { path: '/', component: Home },
-  { path: '/conversation', component: Conversation },
+  { 
+  path: '/', 
+  name: 'Home', 
+  component: Home 
+  },
+  { 
+    path: '/Conversation', 
+    name: 'Conversation', 
+    component: Conversation 
+  },
 ]
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = createRouter({
+  
   // 4. Provide the history implementation to use. We
   // are using the hash history for simplicity here.
   history: createWebHashHistory(),
