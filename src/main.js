1061373be269790f4e8541de07c889afe6f6a4f9
Koussai21 <koussailaribi21@gmail.com>
2024-02-23 18:21:23 +0100

    Hugo: correction main JS

--- src/main.js
+++ src/main.js
@@ -5,9 +5,6 @@ import router from './router/index'
 import vue3GoogleLogin from 'vue3-google-login'
 
 
-createApp(App).use(store).use(router).use(vue3GoogleLogin, {
-                                        clientId: '562452092883-vmh3f4t58rjlr77o71bv0t1p0lhdci4k.apps.googleusercontent.com', 
-                                     }).mount('#app');
 
 createApp(App).use(store).use(router).use(vue3GoogleLogin, {
                                         clientId: '562452092883-vmh3f4t58rjlr77o71bv0t1p0lhdci4k.apps.googleusercontent.com', 
